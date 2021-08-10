9fd21db2470e3ccf7db1cdc82f2500d458560387
Nikolay N <nneshetkin@gmail.com>
2021-08-10 21:16:31 +0300

    2

--- core/src/ru/gb/StarGame.java
+++ core/src/ru/gb/StarGame.java
@@ -1,32 +1,14 @@
-package ru.geekbrains.stargame;
+package ru.gb;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import ru.gb.screen.MenuScreen;
+
+public class StarGame extends Game {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("barabas512.jpg");
+		setScreen(new MenuScreen());
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		//batch.draw(img, 0, 0);
-		batch.draw(img,0,0,640,480);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
